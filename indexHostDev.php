4a6cec5ba57fc9a9ff2ac1b2e91fe29b7857c8de
ValueGuest <arnaud@valueguest.com>
2024-10-22 21:22:43 +0000

    Update from https://github.com/ValueGuest/ValueGuest-UI/commit/c4a89cb38408eb4435b31518631bdad53bbd7875

--- indexHostDev.php
+++ indexHostDev.php
@@ -114,7 +114,7 @@ dataAllGuestbooks = <?php echo $dataAllGuestbooks; ?>;
 
   <app-root></app-root>
   <noscript>Please enable JavaScript to continue using this application.</noscript>
-<script src="runtime.7e7f67db486a4882.js" type="module"></script><script src="polyfills.593235f0c5796786.js" type="module"></script><script src="main.e9cfcc107f6a2aec.js" type="module"></script>
+<script src="runtime.55ab2200008808c9.js" type="module"></script><script src="polyfills.593235f0c5796786.js" type="module"></script><script src="main.e9cfcc107f6a2aec.js" type="module"></script>
 
 
 </body></html>
