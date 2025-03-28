0133d8359ca9196f9bb17165168d4219ccdefddc
ValueGuest <arnaud@valueguest.com>
2025-03-28 15:08:31 +0000

    Update from https://github.com/ValueGuest/ValueGuest-UI/commit/e7048b0d4d7d45e8843b8891f0369608e353c66b

--- indexHost.php
+++ indexHost.php
@@ -151,7 +151,7 @@ if($nameURL == '.well-known'){
 <body>
   <app-root></app-root>
   <noscript>Please enable JavaScript to continue using this application.</noscript>
-<script src="runtime.e06d8c0efabddedf.js" type="module"></script><script src="polyfills.593235f0c5796786.js" type="module"></script><script src="main.22815a94f4b5cd37.js" type="module"></script>
+<script src="runtime.56a27911752bc7cc.js" type="module"></script><script src="polyfills.593235f0c5796786.js" type="module"></script><script src="main.5d748e2b018b611d.js" type="module"></script>
 
 
 </body></html><?php } ?>
--- indexHostDev.php
+++ indexHostDev.php
@@ -114,7 +114,7 @@ dataAllGuestbooks = <?php echo $dataAllGuestbooks; ?>;
 
   <app-root></app-root>
   <noscript>Please enable JavaScript to continue using this application.</noscript>
-<script src="runtime.e06d8c0efabddedf.js" type="module"></script><script src="polyfills.593235f0c5796786.js" type="module"></script><script src="main.22815a94f4b5cd37.js" type="module"></script>
+<script src="runtime.56a27911752bc7cc.js" type="module"></script><script src="polyfills.593235f0c5796786.js" type="module"></script><script src="main.5d748e2b018b611d.js" type="module"></script>
 
 
 </body></html>
