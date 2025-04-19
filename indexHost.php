aea0cc27b6f60913f1344362d20aca129203e798
ValueGuest <arnaud@valueguest.com>
2025-04-19 20:21:10 +0000

    Update from https://github.com/ValueGuest/ValueGuest-UI/commit/086f8f220061a33476b2a94ae4e235e4e45028a9

--- indexHost.php
+++ indexHost.php
@@ -152,7 +152,7 @@ if($nameURL == '.well-known'){
 <body>
   <app-root></app-root>
   <noscript>Please enable JavaScript to continue using this application.</noscript>
-<script src="runtime.fd96b96d876be7b5.js" type="module"></script><script src="polyfills.593235f0c5796786.js" type="module"></script><script src="main.48a5a27c2d180a59.js" type="module"></script>
+<script src="runtime.53e55b88dacaa48a.js" type="module"></script><script src="polyfills.593235f0c5796786.js" type="module"></script><script src="main.48a5a27c2d180a59.js" type="module"></script>
 
 
 </body></html><?php } ?>
